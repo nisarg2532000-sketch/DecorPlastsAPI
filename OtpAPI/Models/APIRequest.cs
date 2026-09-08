6ac3590822bb4944b65874d23e08ddb4feab7d2f
nisarg2532000-sketch <nisarg2532000@gmail.com>
2026-09-08 22:13:32 +0530

    cc

--- OtpAPI/Models/APIRequest.cs
+++ OtpAPI/Models/APIRequest.cs
@@ -320,14 +320,17 @@ namespace OtpAPI.Models
         public string userid { get; set; }
         public string token { get; set; }
         public string OrderId { get; set; }
-        public List<InsertOrderitem> items { get; set; }
+        public List<DeleteOrderitem> items { get; set; }
+    }
+    public class DeleteOrderitem
+    {
+        public string CodeId { get; set; }
     }
     public class InsertOrderitem
     {
         public string CategoryId { get; set; }
         public string CodeId { get; set; }
         public string Quantity { get; set; }
-
     }
     public class UpdateOrder
     {
